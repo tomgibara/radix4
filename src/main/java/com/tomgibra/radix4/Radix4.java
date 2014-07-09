45150464bcccc080d1db04ba8d35c3082943d26b
Tom Gibara <me@tomgibara.com>
2014-07-09 22:29:22 +0100

    Added documentation to public classes and methods.

--- src/main/java/com/tomgibra/radix4/Radix4.java
+++ src/main/java/com/tomgibra/radix4/Radix4.java
@@ -26,6 +26,18 @@ import java.io.Writer;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 
+/**
+ * Provides binary-to-text and text-to-binary functions. It is the entry-point
+ * for accessing the functions provided by this package. Instances of this class
+ * are safe for concurrent use by multiple threads.
+ * 
+ * Unless otherwise indicated, passing a null parameter to any method of this
+ * class will raise an {@link IllegalArgumentException}.
+ * 
+ * @author tomgibara
+ * 
+ */
+
 public final class Radix4 {
 
 	static final Charset ASCII = Charset.forName("ASCII");
@@ -77,12 +89,28 @@ public final class Radix4 {
 	
 	private static final Radix4 instance = new Radix4(Radix4Policy.DEFAULT);
 
+	/**
+	 * Obtain an instance which uses the default policy.
+	 * 
+	 * @return a Radix4 instance
+	 * @see Radix4Policy#DEFAULT
+	 */
+	
 	public static Radix4 use() {
 		return instance;
 	}
 	
+	/**
+	 * Obtain an instance which uses the supplied policy. The policy may be used
+	 * control the operating parameters of the encoding/decoding.
+	 * 
+	 * @return a Radix4 instance
+	 * @see Radix4Policy
+	 */
+	
 	public static Radix4 use(Radix4Policy policy) {
-		return new Radix4(policy.immutableCopy());
+		if (policy == null) throw new IllegalArgumentException("null policy");
+		return policy == Radix4Policy.DEFAULT ? instance : new Radix4(policy.immutableCopy());
 	}
 	
 	private final Radix4Policy policy;
@@ -91,40 +119,123 @@ public final class Radix4 {
 		this.policy = policy;
 	}
 	
+	/**
+	 * The policy under which the Radix4 encoding and decoding are operating.
+	 * 
+	 * @return the policy, never null
+	 */
+	
 	public Radix4Policy getPolicy() {
 		return policy;
 	}
+
+	/**
+	 * Provides encoding from an {@link OutputStream} containing binary data to
+	 * an {@link OutputStream} to which character data will be written.
+	 * 
+	 * @param out
+	 *            an output stream to which Radix4 encoded data should be
+	 *            written
+	 * @return an output stream to which binary data may be written for encoding
+	 */
 	
+	//TODO rename outputToStream
 	public OutputStream createStreamOutput(OutputStream out) {
 		if (out == null) throw new IllegalArgumentException("null out");
 		return new Radix4OutputStream.ByteStream(policy, out);
 	}
 	
+	/**
+	 * Provides encoding from an {@link OutputStream} containing binary data to
+	 * a {@link Writer} to which character data will be written.
+	 * 
+	 * @param writer
+	 *            a writer to which Radix4 encoded data should be written
+	 * @return an output stream to which binary data may be written for encoding
+	 */
+	
+	//TODO rename outputToWriter
 	public OutputStream createWriterOutput(Writer writer) {
 		if (writer == null) throw new IllegalArgumentException("null writer");
 		return new Radix4OutputStream.CharStream(policy, writer);
 	}
 	
+	/**
+	 * Provides encoding from an {@link OutputStream} containing binary data to
+	 * a {@link StringBuilder} to which character data will be appended.
+	 * 
+	 * @param writer
+	 *            a writer to which Radix4 encoded data should be written
+	 * @return an output stream to which binary data may be written for encoding
+	 */
+	
+	//TODO rename outputToBuilder
 	public OutputStream createStringOutput(StringBuilder builder) {
 		if (builder == null) throw new IllegalArgumentException("null builder");
 		return new Radix4OutputStream.Chars(policy, builder);
 	}
 	
+	/**
+	 * Provides decoding from an {@link InputStream} containing Radix4 encoded
+	 * data via an {@link InputStream} from which the decoded binary data may be
+	 * read.
+	 * 
+	 * @param in
+	 *            an input stream from which Radix4 encoded data may be read
+	 * @return an input stream from which the decoded binary data can be read
+	 */
+	
+	//TODO rename inputFromStream
 	public InputStream createStreamInput(InputStream in) {
 		if (in == null) throw new IllegalArgumentException("null in");
 		return new Radix4InputStream.ByteStream(in);
 	}
 	
+	/**
+	 * Provides decoding from a {@link Reader} containing Radix4 encoded
+	 * data via an {@link InputStream} from which the decoded binary data may be
+	 * read.
+	 * 
+	 * @param in
+	 *            an reader from which Radix4 encoded data may be read
+	 * @return an input stream from which the decoded binary data can be read
+	 */
+	
+	//TODO rename inputFromReader
 	public InputStream createReaderInput(Reader reader) {
 		if (reader == null) throw new IllegalArgumentException("null reader");
 		return new Radix4InputStream.CharStream(reader);
 	}
 	
+	/**
+	 * Provides decoding from a {@link CharSequence} containing Radix4 encoded
+	 * data via an {@link InputStream} from which the decoded binary data may be
+	 * read.
+	 * 
+	 * @param in
+	 *            an character sequence from which Radix4 encoded data may be
+	 *            read
+	 * @return an input stream from which the decoded binary data can be read
+	 */
+	
+	//TODO rename inputFromChars
 	public InputStream createStringInput(CharSequence chars) {
 		if (chars == null) throw new IllegalArgumentException("null chars");
 		return new Radix4InputStream.Chars(chars);
 	}
-	
+
+	/**
+	 * Computes the number of ASCII characters required to encode a specified
+	 * number of bytes. The character count includes the terminating sequence if
+	 * one is specified by the policy.
+	 * 
+	 * @param length
+	 *            the number of bytes to be encoded
+	 * @return the number characters required to Radix4 encode the specified
+	 *         number of bytes
+	 */
+
+	//TODO rename length to byteLength
 	public long computeEncodedLength(long length) {
 		long encodedLength = length / 3 * 4;
 
@@ -146,6 +257,14 @@ public final class Radix4 {
 		return encodedLength;
 	}
 	
+	/**
+	 * Encodes a byte array to a string using a Radix4 encoding.
+	 * 
+	 * @param bytes
+	 *            the bytes to encode
+	 * @return a string containing the encoded bytes
+	 */
+
 	public String encodeToString(byte[] bytes) {
 		if (bytes == null) throw new IllegalArgumentException("null bytes");
 		StringBuilder builder = new StringBuilder();
@@ -160,6 +279,15 @@ public final class Radix4 {
 		return builder.toString();
 	}
 	
+	/**
+	 * Encodes a byte array using a Radix4 encoding. The encoded data is
+	 * returned as a byte array of ASCII characters.
+	 * 
+	 * @param bytes
+	 *            the bytes to encode
+	 * @return encoding characters as bytes
+	 */
+
 	public byte[] encodeToBytes(byte[] bytes) {
 		if (bytes == null) throw new IllegalArgumentException("null bytes");
 		long encodedLength = computeEncodedLength(bytes.length);
@@ -176,6 +304,15 @@ public final class Radix4 {
 		return baos.toByteArray();
 	}
 
+	/**
+	 * Decodes a {@link CharSequence} of Radix4 encoded binary data into a byte
+	 * array.
+	 * 
+	 * @param chars
+	 *            the Radix4 encoded data
+	 * @return the decoded data as a byte array
+	 */
+
 	public byte[] decodeToBytes(CharSequence chars) {
 		if (chars == null) throw new IllegalArgumentException("null chars");
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -184,6 +321,14 @@ public final class Radix4 {
 		return out.toByteArray();
 	}
 	
+	/**
+	 * Decodes a byte array containing of Radix4 encoded data into a byte array.
+	 * 
+	 * @param bytes
+	 *            the Radix4 encoded data
+	 * @return the decoded data as a byte array
+	 */
+
 	public byte[] decodeToBytes(byte[] bytes) {
 		if (bytes == null) throw new IllegalArgumentException("null bytes");
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
--- src/main/java/com/tomgibra/radix4/Radix4Policy.java
+++ src/main/java/com/tomgibra/radix4/Radix4Policy.java
@@ -18,10 +18,28 @@ package com.tomgibra.radix4;
 
 import java.io.Serializable;
 
+/**
+ * Provides tailored encoding and decoding by a {@link Radix4}. Attempting to
+ * modify the settings of an immutable policy will raise an
+ * {@link IllegalStateException}.
+ * 
+ * Immutable instances of this class are safe for concurrent access from
+ * multiple threads.
+ * 
+ * @author tomgibara
+ * 
+ */
+
 public final class Radix4Policy implements Serializable {
 
 	private static final long serialVersionUID = 3743746958617647872L;
 
+	/**
+	 * A default policy instance. The default policy declares that no
+	 * line-breaks and no terminating characters will be written (though they
+	 * may still be read).
+	 */
+	
 	public static final Radix4Policy DEFAULT = new Radix4Policy().immutableCopy();
 	
 	static final int NO_LINE_BREAK = 0;
@@ -36,6 +54,10 @@ public final class Radix4Policy implements Serializable {
 	boolean terminated;
 	char terminator;
 	byte terminatorByte;
+	
+	/**
+	 * Creates a new mutable policy with default settings.
+	 */
 	
 	public Radix4Policy() {
 		mutable = true;
@@ -59,19 +81,59 @@ public final class Radix4Policy implements Serializable {
 		this.terminatorByte = that.terminatorByte;
 	}
 	
+	/**
+	 * Specifies the number of bytes used to buffer stream operations. Note that
+	 * currently buffering is only applied to writing and not reading.
+	 * 
+	 * It's possible to restore the default buffer size by supplying a
+	 * non-positive size.
+	 * 
+	 * @param bufferSize
+	 *            the buffer size to use in bytes or a non-positive number to
+	 *            indicate the default size.
+	 */
+	
 	public void setBufferSize(int bufferSize) {
 		if (bufferSize < 1) bufferSize = DEFAULT_BUFFER_SIZE;
 		this.bufferSize = bufferSize;
 	}
+	
+	/**
+	 * The number of bytes used to buffer stream operations
+	 * @return the size in bytes.
+	 */
 
 	public int getBufferSize() {
 		return bufferSize;
 	}
 	
+	/**
+	 * The number of characters output before a line-break is inserted. A
+	 * non-negative line length indicates that no line-breaks should be
+	 * inserted. Note that any whitespace encountered during decoding will be
+	 * skipped irrespective of whether line-breaks are inserted during encoding.
+	 * 
+	 * @param lineLength
+	 *            the length of line into which encoded output should be split.
+	 */
+	
 	public void setLineLength(int lineLength) {
 		checkMutable();
 		this.lineLength = lineLength < 1 ? NO_LINE_BREAK : lineLength;
 	}
+
+	/**
+	 * The character sequence used to delimit lines in encoded output. The
+	 * string must be non-empty and contain only whitespace characters. In this
+	 * context whitespace characters are one of: '\r', '\n', '\t' and '&nbsp;'.
+	 * 
+	 * By default, the line breaks consist of a single character '\n'.
+	 * 
+	 * @param lineBreak
+	 * @throws IllegalArgumentException
+	 *             if the supplied string is null or empty or contains
+	 *             non-whitespace characters.
+	 */
 	
 	public void setLineBreak(String lineBreak) {
 		checkMutable();
@@ -86,39 +148,108 @@ public final class Radix4Policy implements Serializable {
 		lineBreakBytes = lineBreak.getBytes(Radix4.ASCII);
 	}
 
+	/**
+	 * Whether encoded output should be terminated with some number of
+	 * termination characters to unambiguously indicate the end of an encoded
+	 * stream.
+	 * 
+	 * @param terminated
+	 *            whether the output should be terminated
+	 */
+
 	public void setTerminated(boolean terminated) {
 		checkMutable();
 		this.terminated = terminated;
 	}
+
+	/**
+	 * Specifies a character to be used for terminating encodings.
+	 * 
+	 * By default, the character '.' is used to terminate encodings.
+	 * 
+	 * @param terminator
+	 *            a termination character
+	 * @throws IllegalArgumentException
+	 *             if the termination character might appear in an encoding, or
+	 *             as a whitespace
+	 */
 	
 	public void setTerminator(char terminator) {
 		if (!Radix4.isTerminator(terminator)) throw new IllegalArgumentException("invalid terminator");
 		this.terminator = terminator;
 		this.terminatorByte = (byte) terminator;
 	}
-	
+
+	/**
+	 * The number of characters between line breaks in encoded output or zero to
+	 * indicate that line-breaks should not be output.
+	 * 
+	 * @return the line length in ASCII characters, or zero
+	 */
+
 	public int getLineLength() {
 		return lineLength;
 	}
 	
+	/**
+	 * The characters inserted to form a line-break.
+	 * 
+	 * @return a string of one or more whitespace characters
+	 */
+
 	public String getLineBreak() {
 		return lineBreak;
 	}
-	
+
+	/**
+	 * Whether encoded output should be terminated
+	 * 
+	 * @return true iff termination characters should be output
+	 */
+
 	public boolean isTerminated() {
 		return terminated;
 	}
-	
+
+	/**
+	 * The character used to indicate termination
+	 * 
+	 * @return the termination character
+	 */
+
 	public char getTerminator() {
 		return terminator;
 	}
-	
+
+	/**
+	 * Creates a new policy which is identical to this policy and mutable.
+	 * 
+	 * @return a mutable copy of this policy
+	 */
+
 	public Radix4Policy mutableCopy() {
 		return new Radix4Policy(this, true);
 	}
 	
+	/**
+	 * Returns a policy which is identical to this policy and immutable. This
+	 * may or may not result in a new policy object being created.
+	 * 
+	 * @return an immutable copy of this policy
+	 */
+
 	public Radix4Policy immutableCopy() {
 		return mutable ? new Radix4Policy(this, false) : this;
+	}
+
+	/**
+	 * Whether this policy is mutable.
+	 * 
+	 * @return true iff this policy is mutable
+	 */
+
+	public boolean isMutable() {
+		return mutable;
 	}
 	
 	@Override
